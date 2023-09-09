6f21850aaecc9663cb212e87b5dd8d16cd92cc5e
Aditya Godse <adimail2404@gmail.com>
2023-09-10 01:52:34 +0530

    sing along

--- old-macdonald-had-a-farm.cpp
+++ old-macdonald-had-a-farm.cpp
@@ -4,41 +4,85 @@
 
 using namespace std;
 
-class FarmAnimals {
+// Template class for the poem
+template <typename T>
+class OldMacDonald {
 public:
-    FarmAnimals(const string& name, const string& sound)
-        : name(name), sound(sound) {}
+    OldMacDonald(const string& farmName, const string& animalName, const T& sound)
+        : farmName(farmName), animalName(animalName), sound(sound) {}
 
-    string makeSound() const {
-        return sound;
-    }
-
-    string getName() const {
-        return name;
+    void sing() const {
+        cout << farmName << " had a farm" << endl;
+        cout << "Ee i ee i o" << endl;
+        cout << "And on his farm he had some " << animalName << endl;
+        cout << "E-I-E-I-O!" << endl;
+        cout << "With a " << sound << "-" << sound << " here" << endl;
+        cout << "And a " << sound << "-" << sound << " there" << endl;
+        cout << "Here a " << sound << " there a " << sound << endl;
+        cout << "Everywhere " << sound << "-" << sound << endl << endl;
     }
 
 private:
+    string farmName;
+    string animalName;
+    T sound;
+};
+
+// Base class for farm animals
+class FarmAnimal {
+public:
+    FarmAnimal(const string& name, const string& sound)
+        : name(name), sound(sound) {}
+
+    virtual void sing() const {
+        OldMacDonald<string> poem("Old MacDonald", name, sound);
+        poem.sing();
+    }
+
+protected:
     string name;
     string sound;
 };
 
-int main() {
-    vector<FarmAnimals> animal;
-    
-    animal.emplace_back("Cows", "moo");
-    animal.emplace_back("Chicks", "cluck");
-    animal.emplace_back("Pigs", "oink");
-    animal.emplace_back("ducks","quack");
+// Derived classes for specific animals
+class Cow : public FarmAnimal {
+public:
+    Cow() : FarmAnimal("Cows", "moo") {}
+};
 
-    for (const FarmAnimals& animal : animal) {
-        cout << "Old MacDonald had a farm\n";
-        cout << "Ee i ee i o\n";
-        cout << "And on his farm he had some " << animal.getName() << endl;
-        cout << "E-I-E-I-O!\n";
-        cout << "With a " << animal.makeSound() << "-" << animal.makeSound() << " here\n";
-        cout << "And a " << animal.makeSound() << "-" << animal.makeSound() << " there\n";
-        cout << "Here a " << animal.makeSound() << " there a " << animal.makeSound() << endl;
-        cout << "Everywhere " << animal.makeSound() << "-" << animal.makeSound() << endl << endl;
+class Chicken : public FarmAnimal {
+public:
+    Chicken() : FarmAnimal("Chicks", "cluck") {}
+};
+
+class Pig : public FarmAnimal {
+public:
+    Pig() : FarmAnimal("Pigs", "oink") {}
+};
+
+class Duck : public FarmAnimal {
+public:
+    Duck() : FarmAnimal("Ducks", "quack") {}
+};
+
+int main() {
+    vector<FarmAnimal*> animals;
+
+    // Create instances of specific animal classes
+    Cow cow;
+    Chicken chicken;
+    Pig pig;
+    Duck duck;
+
+    // Store the animals in a vector
+    animals.push_back(&cow);
+    animals.push_back(&chicken);
+    animals.push_back(&pig);
+    animals.push_back(&duck);
+
+    // Sing the Old MacDonald poem for each animal
+    for (const FarmAnimal* animal : animals) {
+        animal->sing();
     }
 
     return 0;
